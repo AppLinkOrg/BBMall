2791c4f8020a9e0859b6c1870c2f07ab99a7c1b6
alucard263096 <alucard263096@126.com>
2019-03-19 08:02:33 +0800

    commit

--- myApp/src/app/story/story.page.ts
+++ myApp/src/app/story/story.page.ts
@@ -3,6 +3,7 @@ import { AppBase } from '../AppBase';
 import { Router } from '@angular/router';
 import { NavController, ModalController, ToastController, AlertController, NavParams } from '@ionic/angular';
 import { GoodsApi } from 'src/providers/goods.api';
+import { AppUtil } from '../app.util';
 
 @Component({
   selector: 'app-story',
@@ -21,8 +22,40 @@ export class StoryPage extends AppBase {
     super(router, navCtrl, modalCtrl, toastCtrl, alertCtrl);
   }
 
-  onMyLoad() {
-    alert(this.navParams.get("id"));
-  }
+  story = null;
+  goods = [];
+  rstory = [];
+  showcov=false;
+  scrolltop=0;
 
+  onMyLoad(){
+    var id=this.navParams.get("id");
+    this.goodsApi.story({id:id}).then((story)=>{
+      
+      story.content = AppUtil.HtmlDecode(story.content);
+      this.story=story;
+      this.goodsApi.storygoods({story_id:id}).then((goods)=>{
+        this.goods=goods;
+      });
+      this.goodsApi.storystory({story_id:id}).then((rstory)=>{
+        this.rstory=rstory;
+      });
+      setTimeout(()=>{
+        this.showcov=true;
+      },2000);
+    });
+
+
+    
+  }
+  logScrollStart(){
+    console.log("logScrollStart");
+  }
+  logScrolling(e){
+    console.log(e);
+    this.scrolltop=e.detail.scrollTop;
+  }
+  logScrollEnd(){
+    console.log("logScrollEnd");
+  }
 }
